36fadcf1ebc2d9d96a95ce2c9cf578a5f96714b5
karan <karan@zibtek.in>
2019-04-08 17:34:36 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multiple-forms';
+  seriesAnswerTypeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createAnswerSeriesForm();
+  }
+
+  createAnswerSeriesForm() {
+    this.seriesAnswerTypeForm = this.fb.group({
+      answerSeriesRows: this.fb.array([])
+    });
+    this.seriesAnswerTypeForm.setControl('answerSeries', this.fb.array([]))
+  }
+
+  get answerSeries(): FormArray {
+    return this.seriesAnswerTypeForm.get('answerSeries') as FormArray;
+  }
+
+  addNewAnswerRow() {
+    this.answerSeries.push(this.initAnswerSeriesRow());
+  }
+
+  deleteAnswerRow(index: number) {
+    this.answerSeries.removeAt(index);
+  }
+
+  initAnswerSeriesRow() {
+    return this.fb.group({
+      displayText: [''],
+      answerLabel: ['']
+    });
+  }
+
+  ngOnInit() {
+
+  }
 }
